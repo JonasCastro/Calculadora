8407cabe64980c7bdc16c7d9bbc9ef07f2d2045d
Jonas Castro <jonascastro128@gmail.com>
2017-09-02 13:50:59 -0300

    #Transicao entre layouts no fragmento molaridade atravez do FloatingButton esta funcionando - metodos para a segunda formula da molaridade esta ok

--- app/src/main/java/fragment/Molaridade.java
+++ app/src/main/java/fragment/Molaridade.java
@@ -23,7 +23,7 @@ import static android.view.View.*;
 public class Molaridade extends Fragment {
 
     private Button layoutCL_btnIgualdade,layoutCL_btnLimpar,layoutFn_btnIgualdade,layoutFn_btnLimpar;
-    private EditText layoutCL_edTxtMolaridade,layoutCL_edTxtNMols,layoutCL_edTxtVolume,layoutCL_edTxtMassa,layoutCL_edTxtMassaM;
+    private EditText layoutCL_edTxtMolaridade,layoutCL_edTxtVolume,layoutCL_edTxtMassa,layoutCL_edTxtMassaM;
     private EditText layoutFn_edTxtMolaridade,layoutFn_edTxtNMols,layoutFn_edTxtVolume;
     private FloatingActionButton fab_menu,fab_calc,fab_fornec;
     private ConstraintLayout layoutFornecNmols,layoutCalcNmols;
@@ -50,18 +50,65 @@ public class Molaridade extends Fragment {
     private void buildLayoutCalcularNmols(View view){
 
         layoutCL_btnLimpar = (Button) view.findViewById(R.id.fmML_layoutCalcnMols_btnLimpar);
+        layoutCL_btnIgualdade= (Button) view.findViewById(R.id.fmML_layoutCalcnMols_btnIgualdade);
 
         layoutCL_edTxtMolaridade = (EditText) view.findViewById(R.id.fmML_layoutCalcnMols_editTextMolaridade);
-        layoutCL_edTxtNMols = (EditText) view.findViewById(R.id.fmML_layoutCalcnMols_editTextNMols);
         layoutCL_edTxtVolume = (EditText) view.findViewById(R.id.fmML_layoutCalcnMols_editTextVolume);
         layoutCL_edTxtMassa = (EditText) view.findViewById(R.id.fmML_layoutCalcnMols_editTextMassa);
         layoutCL_edTxtMassaM = (EditText) view.findViewById(R.id.fmML_layoutCalcnMols_editTextMassaM);
 
+        layoutCL_btnIgualdade.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Double result ;
+                if(!layoutCL_edTxtVolume.getText().toString().isEmpty() && !layoutCL_edTxtMassa.getText().toString().isEmpty() && !layoutCL_edTxtMassaM.getText().toString().isEmpty()) {
+
+                    Double volume = Double.parseDouble(layoutCL_edTxtVolume.getText().toString());
+                    Double massa = Double.parseDouble(layoutCL_edTxtMassa.getText().toString());
+                    Double massaM = Double.parseDouble(layoutCL_edTxtMassaM.getText().toString());
+
+                    result = massa/(massaM*volume);
+                    layoutCL_edTxtMolaridade.setText(String.format("%.2f" ,result).replace(",","."));
+                }
+
+                else if(!layoutCL_edTxtMolaridade.getText().toString().isEmpty() && layoutCL_edTxtVolume.getText().toString().isEmpty() && !layoutCL_edTxtMassa.getText().toString().isEmpty() && !layoutCL_edTxtMassaM.getText().toString().isEmpty()) {
+
+                    Double molaridade = Double.parseDouble(layoutCL_edTxtMolaridade.getText().toString());
+                    Double massa = Double.parseDouble(layoutCL_edTxtMassa.getText().toString());
+                    Double massaM = Double.parseDouble(layoutCL_edTxtMassaM.getText().toString());
+
+                    result = massa/(molaridade*massaM);
+                    layoutCL_edTxtVolume.setText(String.format("%.2f" ,result).replace(",","."));
+                }
+                else if(!layoutCL_edTxtMolaridade.getText().toString().isEmpty() && !layoutCL_edTxtVolume.getText().toString().isEmpty() && layoutCL_edTxtMassa.getText().toString().isEmpty() && !layoutCL_edTxtMassaM.getText().toString().isEmpty()) {
+
+                    Double molaridade = Double.parseDouble(layoutCL_edTxtMolaridade.getText().toString());
+                    Double volume = Double.parseDouble(layoutCL_edTxtVolume.getText().toString());
+                    Double massaM = Double.parseDouble(layoutCL_edTxtMassaM.getText().toString());
+
+                    result = massaM*molaridade*volume;
+                    layoutCL_edTxtMassa.setText(String.format("%.2f" ,result).replace(",","."));
+                }
+                else if(!layoutCL_edTxtMolaridade.getText().toString().isEmpty() && !layoutCL_edTxtVolume.getText().toString().isEmpty() && !layoutCL_edTxtMassa.getText().toString().isEmpty() && layoutCL_edTxtMassaM.getText().toString().isEmpty()) {
+
+                    Double molaridade = Double.parseDouble(layoutCL_edTxtMolaridade.getText().toString());
+                    Double volume = Double.parseDouble(layoutCL_edTxtVolume.getText().toString());
+                    Double massa = Double.parseDouble(layoutCL_edTxtMassa.getText().toString());
+
+                    result = massa/(molaridade*volume);
+                    layoutCL_edTxtMassaM.setText(String.format("%.2f" ,result).replace(",","."));
+                }
+                else {
+                    Toast.makeText(getActivity().getApplicationContext(),"Forneça Três Valores.",Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
         layoutCL_btnLimpar.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 layoutCL_edTxtMolaridade.setText("");
-                layoutCL_edTxtNMols.setText("");
                 layoutCL_edTxtVolume.setText("");
                 layoutCL_edTxtMassa.setText("");
                 layoutCL_edTxtMassaM.setText("");
@@ -161,21 +208,25 @@ public class Molaridade extends Fragment {
                     layoutFornecNmols.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.layout_molaridade_close));
                     layoutCalcNmols.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.layout_molaridade_open));
 
-                    layoutFornecNmols.setClickable(false);
-                    layoutFn_edTxtMolaridade.setClickable(false);
-                    layoutFn_edTxtNMols.setClickable(false);
-                    layoutFn_edTxtVolume.setClickable(false);
-                    layoutFn_btnIgualdade.setClickable(false);
-                    layoutFn_btnLimpar.setClickable(false);
+                    layoutFornecNmols.setVisibility(INVISIBLE);
 
-                    layoutCalcNmols.setClickable(true);
-                    layoutCL_edTxtMolaridade.setClickable(true);
-                    layoutCL_edTxtNMols.setClickable(true);
-                    layoutCL_edTxtVolume.setClickable(true);
-                    layoutCL_edTxtMassa.setClickable(true);
-                    layoutCL_edTxtMassaM.setClickable(true);
-//                    layoutCL_btnIgualdade.setClickable(true);
-//                    layoutCL_btnLimpar.setClickable(true);
+                    layoutFn_edTxtMolaridade.setVisibility(INVISIBLE);
+                    layoutFn_edTxtNMols.setVisibility(INVISIBLE);
+                    layoutFn_edTxtVolume.setVisibility(INVISIBLE);
+                    layoutFn_btnIgualdade.setVisibility(INVISIBLE);
+                    layoutFn_btnLimpar.setVisibility(INVISIBLE);
+
+                    layoutCalcNmols.setVisibility(VISIBLE);
+
+                    layoutCL_edTxtMolaridade.setVisibility(VISIBLE);
+                    layoutCL_edTxtVolume.setVisibility(VISIBLE);
+                    layoutCL_edTxtMassa.setVisibility(VISIBLE);
+                    layoutCL_edTxtMassaM.setVisibility(VISIBLE);
+                    layoutCL_btnIgualdade.setVisibility(VISIBLE);
+                    layoutCL_btnLimpar.setVisibility(VISIBLE);
+
+
+
 
                     isLayoutCalOpen = true;
                 }
@@ -191,21 +242,23 @@ public class Molaridade extends Fragment {
                     layoutCalcNmols.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.layout_molaridade_close));
                     layoutFornecNmols.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.layout_molaridade_open));
 
-                    layoutCalcNmols.setClickable(false);
-                    layoutCL_edTxtMolaridade.setClickable(false);
-                    layoutCL_edTxtNMols.setClickable(false);
-                    layoutCL_edTxtVolume.setClickable(false);
-                    layoutCL_edTxtMassa.setClickable(false);
-                    layoutCL_edTxtMassaM.setClickable(false);
-//                    layoutCL_btnIgualdade.setClickable(false);
-//                    layoutCL_btnLimpar.setClickable(false);
+                    layoutCalcNmols.setVisibility(INVISIBLE);
 
-                    layoutFornecNmols.setClickable(true);
-                    layoutFn_edTxtMolaridade.setClickable(true);
-                    layoutFn_edTxtNMols.setClickable(true);
-                    layoutFn_edTxtVolume.setClickable(true);
-                    layoutFn_btnIgualdade.setClickable(true);
-                    layoutFn_btnLimpar.setClickable(true);
+                    layoutCL_edTxtMolaridade.setVisibility(INVISIBLE);
+                    layoutCL_edTxtVolume.setVisibility(INVISIBLE);
+                    layoutCL_edTxtMassa.setVisibility(INVISIBLE);
+                    layoutCL_edTxtMassaM.setVisibility(INVISIBLE);
+                    layoutCL_btnIgualdade.setVisibility(INVISIBLE);
+                    layoutCL_btnLimpar.setVisibility(INVISIBLE);
+
+                    layoutFornecNmols.setVisibility(VISIBLE);
+
+                    layoutFn_edTxtMolaridade.setVisibility(VISIBLE);
+                    layoutFn_edTxtNMols.setVisibility(VISIBLE);
+                    layoutFn_edTxtVolume.setVisibility(VISIBLE);
+                    layoutFn_btnIgualdade.setVisibility(VISIBLE);
+                    layoutFn_btnLimpar.setVisibility(VISIBLE);
+
 
                     isLayoutCalOpen = false;
                 }
